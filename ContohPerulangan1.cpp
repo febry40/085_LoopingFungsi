62370ef50907b4d7221aee37fc18ad5252f4bb9f
febry40 <febryevayonisa@gmail.com>
2024-03-18 14:28:21 +0700

    looping menggunakan for

---
+++ ContohPerulangan1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namse space std;
+
+int main()
+{
+    int i;
+    int arr [5;
+    
+    for (i = 0; 1 < 5; i++)
+    {
+        cout << i << "i" << "Febry" << endl;
+    }
+
+    for (i = 0; < 5; i ++){
+        cout << "Febry = ";
+        cin >> arr[i];
+    }
+
+    for(i = 0; i<5;i++) {
+        cout << "Febry ke - " << i << "i" << arr [i] << endl;
+    }    
+}
