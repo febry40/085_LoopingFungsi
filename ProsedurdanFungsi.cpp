671985dc49214c51b0deb0cbc605c3a725879f1f
febry40 <febryevayonisa@gmail.com>
2024-03-18 16:02:29 +0700

    prosedur fungsi menggunakan perulangan do...while

---
+++ ProsedurdanFungsi.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+string nama;
+int nXpander, nPorsche, nAvanza, nBrio, nLamborghini;
+int hXpander = 300, hPorsche = 8000, hAvanza = 250, hBrio = 200, hLamborghini = 6000;
+
+ int input(){
+     cout << "Nama Pembeli : ";
+     cin >> nama;
+     cout << "Jumlah Xpander:";
+     cin >> nXpander;
+
+     cout << "Jumlah Porsche :";
+     cin >> nPorsche;
+
+     cout << "Jumlah Avanza";
+     cin >> nAvanza;
+
+     cout << "Jumlah Brio";
+     cin >> nBrio;
+
+     cout << "Jumlah lamborghini";
+     cin >> nLamborghini;
+ }     
+     
+int TotalHarga(){
+    return (nXpander * hXpander) + (nPorsche * hPorsche) + (nAvanza * hAvanza) + (nBrio * hBrio) + (nLamborghini * hLamborghini);
+}
+
+void Display(){
+    cout << "Total Harganya :"  << TotalHarga() << endl;
+}
+
+int main(){
+    char pilihan;
+    do{
+    input();
+    Display();
+    cout << "Apakah ingin membeli kembali ? ";
+    cin >> pilihan;
+    }while(pilihan == 'y' ||pilihan == 'Y');
+}
